bbe58a872b48e202fb3b3e6374e684e54b6d5b16
Emily Rodriguez <em.rodriguez@correo.unimet.edu.ve>
2023-11-09 17:40:46 -0400

    metodos principales grafo + vertices+aristas

--- GraphAdyMatrix.py
+++ GraphAdyMatrix.py
@@ -1,18 +1,63 @@
+from Vertex import Vertex
 class GraphAdyMatrix:
     def __init__(self, num_of_nodes, directed):
         self.m_num_of_nodes = num_of_nodes
         self.m_directed = directed
         self.m_adj_matrix = [[0 for column in range(num_of_nodes)] for row in range(num_of_nodes)]
+        self.size = 0
+        self.vertexList = []
 
+    def is_empty(self):
+        if self.size == 0:
+            return True
+        else:
+            return False
+    
     def add_edge(self, node1, node2, weight=1):
-        self.m_adj_matrix[node1][node2] = weight
+        exists = self.edge_exists(node1,node2)
 
-        if not self.m_directed:
-            self.m_adj_matrix[node2][node1] = weight
+        if node1 != node2 and not exists:
+            self.m_adj_matrix[node1][node2] = weight
+
+            if not self.m_directed:
+                self.m_adj_matrix[node2][node1] = weight
+
+    def edge_exists(self,node1,node2):
+        exists = False
+        if self.m_adj_matrix[node1][node2] != 0:
+            exists = True
+        
+        return exists
+    
+    def remove_edge(self, node1,node2):
+        if self.m_adj_matrix[node1][node2] == 0:
+            print("no existe la arista")
+        else:
+            self.m_adj_matrix[node1][node2]=0
+            self.m_adj_matrix[node2][node1]=0
+
+    def create_vertex(self,nom,is_couple):
+        verti = Vertex(nom,self.size,is_couple)
+        self.vertexList.append(verti)
+        self.size += 1
+
+    def search_index(self,nom):
+        
+        exists = False
+        i = 0
+        while i < self.size and not exists:
+            if nom == self.vertexList[i].name:
+                exists = True 
+            else:
+                 i += 1;
+        
+        if exists:
+            return i
+        else:
+            return -1 
 
     def print_adj_matrix(self):
         for row in range(len(self.m_adj_matrix)):
             print(self.m_adj_matrix[row])
        
-        
-        
+   
--- main.py
+++ main.py
@@ -3,15 +3,31 @@ from GraphAdyMatrix import GraphAdyMatrix
 def main():
     grafo = GraphAdyMatrix(36,False)
 
-    grafo.add_edge(0,1)
-    grafo.add_edge(0,6)
-    grafo.add_edge(1,7)
-    grafo.add_edge(1,2)
-    grafo.add_edge(2,3)
-    grafo.add_edge(2,8)
+    vertex_list = [["0",False],["1",False],["2",False],["3",False],["4",False],["5",False],["6",False],["Javier",False],["8",False],["9",False],
+                   ["Bar",True],["11",False],["12",False],["13", False],["14",False],["15",False],["16",False],["17", False],["18", False],
+                   ["19", False],["Andreina", False],["21", False],["22", False],["23", False],["24", False],["25", False],["26", False],
+                   ["27", False],["28", False],["29", False],["30", False],["Disco", True],["32", False],["Cerveceria", True],["34", False],
+                   ["Cafe", True]]
+    edges_list = [[0,1],[0,6],[1,7],[1,2],[2,3],[2,8],[3,4],[3,9],[4,5],[4,10],[5,11],[6,7],[6,12],[7,8],[7,13],
+                  [8,9],[8,14],[9,10],[9,15],[10,11],[10,16],[11,17],[12,13],[12,18],[13,14],[13,19],[14,15],[14,20],
+                  [15,16],[15,21],[16,17],[16,22],[17,23],[18,19],[18,24],[19,20],[19,25],[20,21],[20,26],[21,22],[21,27],[22,23],[22,28],
+                  [23,29],[24,25],[24,30],[25,26],[25,31],[26,27],[26,32],[27,28],[27,33],[28,29],[28,34],[29,35],[30,31],[31,32],[32,33],
+                  [33,34],[34,35]]
+
+    for vertex in range(len(vertex_list)):
+        min_vertex_list = vertex_list[vertex]
+        grafo.create_vertex(min_vertex_list[0],min_vertex_list[1])
+
+    for edgePar in range(len(edges_list)):
+        min_edge_list = edges_list[edgePar]
+        grafo.add_edge(min_edge_list[0],min_edge_list[1])
     
 
     grafo.print_adj_matrix()
+    print("")
+    print(f"VERTICES {len(grafo.vertexList)}")
+    # for ver in range(len(grafo.vertexList)):
+    #     print(grafo.vertexList[ver].name)
 
 
 main()
