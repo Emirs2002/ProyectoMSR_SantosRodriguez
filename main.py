07f0a4758c8f8d2ea913b5fa74062713b3a502de
Emily Rodriguez <em.rodriguez@correo.unimet.edu.ve>
2023-11-22 12:43:24 -0400

    grafo aristas + networkx

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from GraphAdyMatrix import GraphAdyMatrix
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def main():
     grafo = GraphAdyMatrix(36,False)
 
+    G = nx.Graph()
+    
     vertex_list = [["0",False],["1",False],["2",False],["3",False],["4",False],["5",False],["6",False],["Javier",False],["8",False],["9",False],
                    ["Bar",True],["11",False],["12",False],["13", False],["14",False],["15",False],["16",False],["17", False],["18", False],
                    ["19", False],["Andreina", False],["21", False],["22", False],["23", False],["24", False],["25", False],["26", False],
@@ -13,19 +17,26 @@ def main():
                   [15,16],[15,21],[16,17],[16,22],[17,23],[18,19],[18,24],[19,20],[19,25],[20,21],[20,26],[21,22],[21,27],[22,23],[22,28],
                   [23,29],[24,25],[24,30],[25,26],[25,31],[26,27],[26,32],[27,28],[27,33],[28,29],[28,34],[29,35],[30,31],[31,32],[32,33],
                   [33,34],[34,35]]
-
-    for vertex in range(len(vertex_list)):
-        min_vertex_list = vertex_list[vertex]
-        grafo.create_vertex(min_vertex_list[0],min_vertex_list[1])
+    
+   
+    # for vertex in range(len(vertex_list)):
+    #     min_vertex_list = vertex_list[vertex]
+    #     grafo.create_vertex(min_vertex_list[0],min_vertex_list[1])
+        
 
     for edgePar in range(len(edges_list)):
         min_edge_list = edges_list[edgePar]
-        grafo.add_edge(min_edge_list[0],min_edge_list[1])
+        #grafo.add_edge(min_edge_list[0],min_edge_list[1])
+        G.add_nodes_from(min_edge_list)
+        G.add_edge(min_edge_list[0],min_edge_list[1])
     
+    nx.draw(G,with_labels=True)
+    plt.show()
 
-    grafo.print_adj_matrix()
-    print("")
-    print(f"VERTICES {len(grafo.vertexList)}")
+
+    # grafo.print_adj_matrix()
+    # print("")
+    # print(f"VERTICES {len(grafo.vertexList)}")
     # for ver in range(len(grafo.vertexList)):
     #     print(grafo.vertexList[ver].name)
 
