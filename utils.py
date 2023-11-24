76ef3de92e6a8925fac3c0b3023aee7a0770f25c
Emily Rodriguez <em.rodriguez@correo.unimet.edu.ve>
2023-11-23 20:23:41 -0400

    grafo pintado

--- utils.py
+++ utils.py
@@ -4,12 +4,17 @@ import matplotlib.pyplot as plt
 #GRAFICAR EL GRAFO CON SUS PESOS
 def graficar_grafo(G):
 
-    pos = nx.spring_layout(G, seed=7)
+    pos = nx.spring_layout(G, seed = 7)
 
     #draw nodes
     nx.draw_networkx_nodes(G, pos, node_size=900, node_color="tab:blue")
     nx.draw_networkx_nodes(G, pos, node_size=900, nodelist=["Javier"], node_color="tab:orange")
-    nx.draw_networkx_nodes(G, pos, node_size=900, nodelist=[10], node_color="tab:red")
+    nx.draw_networkx_nodes(G, pos, node_size=900, nodelist=["Andreina"], node_color="tab:pink")
+
+    nx.draw_networkx_nodes(G, pos, node_size=900, nodelist=["Bar"], node_color="tab:red")
+    nx.draw_networkx_nodes(G, pos, node_size=900, nodelist=["Cafe"], node_color="whitesmoke")
+    nx.draw_networkx_nodes(G, pos, node_size=900, nodelist=["Disco"], node_color="tab:purple")
+    nx.draw_networkx_nodes(G, pos, node_size=900, nodelist=["Cerveceria"], node_color="yellow")
 
     #draw edges
     elist = [(u,v) for (u,v,d) in G.edges(data = True)]
@@ -22,7 +27,7 @@ def graficar_grafo(G):
     nx.draw_networkx_edge_labels(G,pos,edge_labels)
     
     ax = plt.gca()
-    ax.margins(0.01)
+    ax.margins(0.08)
     plt.tight_layout()
     plt.axis("off")
     plt.show()
